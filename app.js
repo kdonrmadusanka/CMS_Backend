bb745c16891a108a1581203bcf36084fb870211a
kdonrmadusanka <ravmkdon@gmail.com>
2025-03-29 22:53:25 +0530

    Start the server and listen on the assigned port.

--- app.js
+++ app.js
@@ -1,9 +1,13 @@
 //imports
 import express from 'express';
+import { PORT } from './config/env.js';
 
 const app =  express();
 
 //middleware
 app.use(express.json);
 
-
+// start the server and listen on the assigned port
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`)
+});
