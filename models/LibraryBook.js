f66b237e8b579e76236c3ab78888e7a49eb06f63
kdonrmadusanka <ravmkdon@gmail.com>
2025-03-29 23:49:26 +0530

    Create Library Book model.

---
+++ models/LibraryBook.js
@@ -0,0 +1,15 @@
+const mongoose = require('mongoose');
+const { Schema } = mongoose;
+
+const libraryBookSchema = new Schema({
+  bookId: { type: String, required: true, unique: true },
+  title: { type: String, required: true },
+  author: { type: String, required: true },
+  isbn: { type: String, required: true },
+  publisher: { type: String },
+  category: { type: String },
+  availableCopies: { type: Number, default: 1 },
+  totalCopies: { type: Number, required: true }
+}, { timestamps: true });
+
+module.exports = mongoose.model('LibraryBook', libraryBookSchema);
