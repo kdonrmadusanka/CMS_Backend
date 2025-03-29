796fa83ba9b9a9f7bbeccb5a3221e4d9fab93eaa
kdonrmadusanka <ravmkdon@gmail.com>
2025-03-29 23:45:36 +0530

    Create Course model.

---
+++ models/Course.js
@@ -0,0 +1,19 @@
+const mongoose = require('mongoose');
+const { Schema } = mongoose;
+
+const courseSchema = new Schema({
+  courseCode: { type: String, required: true, unique: true },
+  courseName: { type: String, required: true },
+  description: { type: String },
+  creditHours: { type: Number, required: true },
+  department: { type: String, required: true },
+  instructor: { type: Schema.Types.ObjectId, ref: 'Faculty' },
+  schedule: {
+    days: [{ type: String }],
+    startTime: { type: String },
+    endTime: { type: String },
+    room: { type: String }
+  }
+}, { timestamps: true });
+
+module.exports = mongoose.model('Course', courseSchema);
