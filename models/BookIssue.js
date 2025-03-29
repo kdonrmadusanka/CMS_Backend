04b72d0f9defdb3c4824590687c910b9901e446b
kdonrmadusanka <ravmkdon@gmail.com>
2025-03-29 23:50:18 +0530

    Create Book Issue model.

---
+++ models/BookIssue.js
@@ -0,0 +1,14 @@
+const mongoose = require('mongoose');
+const { Schema } = mongoose;
+
+const bookIssueSchema = new Schema({
+  book: { type: Schema.Types.ObjectId, ref: 'LibraryBook', required: true },
+  student: { type: Schema.Types.ObjectId, ref: 'Student', required: true },
+  issueDate: { type: Date, default: Date.now },
+  dueDate: { type: Date, required: true },
+  returnDate: { type: Date },
+  status: { type: String, enum: ['Issued', 'Returned', 'Overdue'], default: 'Issued' },
+  fine: { type: Number, default: 0 }
+}, { timestamps: true });
+
+module.exports = mongoose.model('BookIssue', bookIssueSchema);
