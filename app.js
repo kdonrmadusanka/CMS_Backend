a0a67b5a18a6e28941f839b3f372fd15ffe32408
kdonrmadusanka <ravmkdon@gmail.com>
2025-03-29 23:17:23 +0530

    Start a connection with mongodb database.

--- app.js
+++ app.js
@@ -1,11 +1,17 @@
 //imports
 import express from 'express';
 import { PORT } from './config/env.js';
+import connectDB from './config/db.js';
 
 const app =  express();
 
 //middleware
 app.use(express.json);
+
+//routes
+
+
+connectDB();
 
 // start the server and listen on the assigned port
 app.listen(PORT, () => {
---
+++ config/db.js
@@ -0,0 +1,14 @@
+import mongoose from "mongoose";
+import { MONGO_URI } from "./env.js";
+
+const connectDB = async () => {
+    try{
+        await mongoose.connect(MONGO_URI);
+        console.log("MongoDB connected.");
+    } catch {
+        console.error(`Error ${error.message}`);
+        process.exit(1);
+    }
+}
+
+export default connectDB;
