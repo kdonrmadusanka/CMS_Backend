5afa4df26e725e8b50b2f09a65b9eba4496c192d
kdonrmadusanka <ravmkdon@gmail.com>
2026-04-04 20:59:21 +0530

    Complete the Auth implementation.

--- app.js
+++ app.js
@@ -15,6 +15,9 @@ const PORT = process.env.PORT || 5000; // default to 5000 if PORT not defined
 app.use(cors());
 app.use(express.json()); // parse JSON request bodies
 
+// Routes
+app.use('/api/auth', authRoutes);
+
 // Connect to database
 connectDB();
 
