d37a45c1a2dd0c4c98f70cccdb868b9143e51cd2
kdonrmadusanka <ravmkdon@gmail.com>
2026-04-07 11:57:36 +0530

    Create the Rest of the models.

---
+++ models/Course.js
@@ -0,0 +1,22 @@
+import mongoose from 'mongoose';
+
+const courseSchema = new mongoose.Schema({
+  name: { type: String, required: true },
+  code: { type: String, required: true, unique: true },
+  description: { type: String },
+  schedule: {
+    day: { type: String, enum: ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'], required: true },
+    startTime: { type: String, required: true }, 
+    endTime: { type: String, required: true },   
+  },
+  instructor: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
+  students: [{ type: mongoose.Schema.Types.ObjectId, ref: 'User' }],
+  lectureMaterials: [{ type: String }],
+  error: { type: String },
+  createdAt: { type: Date, default: Date.now },
+});
+
+
+const Course = mongoose.model('Course', courseSchema);
+
+export default Course;
---
+++ models/Event.js
@@ -0,0 +1,16 @@
+import mongoose from 'mongoose';
+
+const eventSchema = new mongoose.Schema({
+  title: { type: String, required: true },
+  description: { type: String, required: true },
+  date: { type: Date, required: true },
+  location: { type: String, required: true },
+  organizer: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
+  attendees: [{ type: mongoose.Schema.Types.ObjectId, ref: 'User' }],
+  attendeesCount: { type: Number, default: 0 },
+  createdAt: { type: Date, default: Date.now },
+});
+
+const Event = mongoose.model('Event', eventSchema);
+
+export default Event;
